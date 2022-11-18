2e03b9808e4a0af6ea0ccda0caf1a384599559b0
alanaquinoslv <alan.silva@sptech.school>
2022-11-18 11:01:12 -0300

    update

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "americano",
-    user: "root",
-    password: "rq@ln082@",
+    database: "football",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
