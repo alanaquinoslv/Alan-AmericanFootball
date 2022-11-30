60b3625813866c4a4a01871005810c44ab235301
alanaquinoslv <alan.silva@sptech.school>
2022-11-30 11:10:56 -0300

    update videos

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "football",
-    user: "root",
-    password: "rq@ln082@",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
